ca637c2296a6061300521cb07adfd01103e08108
hiroakni <hiroakni@gmail.com>
2018-09-29 19:43:41 +0900

    コメント：1回目のコミット

---
+++ SCSHomePage/Models/Code.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCSHomePage.Models
+{
+    public class Code
+    {
+        [Required]
+        [DisplayName("お名前")]
+        public string Oname { get; set; }
+
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [DisplayName("電話番号")]
+        public int Phone { get; set; }
+
+        [Required]
+        [DisplayName("内容")]
+        public string Detail { get; set; }
+
+    }
+}
---
+++ SCSHomePage/Models/Todo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        [Required]
+        [DisplayName("登録No")]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("名前")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("内容")]
+        public string Detail { get; set; }
+
+        [Required]
+        [DisplayName("パスワード")]
+        public string Pass { get; set; }
+
+        [DisplayName("完了")]
+        public bool Done { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
